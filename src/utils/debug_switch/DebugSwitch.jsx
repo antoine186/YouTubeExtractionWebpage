7647a8539bad61c53c6f9662344f664798588512
Antoine Yi-Cheng Tian <33133769+antoine186@users.noreply.github.com>
2024-04-11 15:28:25 +0100

    Update DebugSwitch.jsx

--- src/utils/debug_switch/DebugSwitch.jsx
+++ src/utils/debug_switch/DebugSwitch.jsx
@@ -1,11 +1,11 @@
 
 export const debugSwitchedOn = true
 
-export const stripePublicKeyProd = 'pk_live_51MyG0LFAAs2DFWSVTtM2eTdG4mfJiRudWfsLF0ilmaQwOOZ2nufVf5SlkLxMVFV5d5WbPAfG948Jav5TedJIvSS800aqnFMq5r'
-export const stripePublicKeyDebug = 'pk_test_51MyG0LFAAs2DFWSVSYjOg0bO20EGAVi3LDtGBypqn3ScKzcn9fV9FpC0QmMHWNw9L831L3R1X7xo4Xf6K2yplsh600khIw33yT'
+export const stripePublicKeyProd = ''
+export const stripePublicKeyDebug = ''
 
-export const basicSubscriptionPriceIdProd = 'price_1Ng88sFAAs2DFWSVFHVrLk32'
-export const basicSubscriptionPriceIdDebug = 'price_1N2QuiFAAs2DFWSVb5cdYrOM'
+export const basicSubscriptionPriceIdProd = ''
+export const basicSubscriptionPriceIdDebug = ''
 
 export const backendUrlProd = 'https://emomachinesbackend.xyz/api'
 export const backendUrlDebug = 'http://localhost:80/api'
